188b5da814809606f0b021c7712086009b718641
rafiazharr17 <rafipilo17@gmail.com>
2023-03-30 10:57:08 +0700

    menukar element pada index x dengan element pada index y

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -29,4 +29,12 @@ void input() {
 		cin >> arr[i];
 	}
 }
+//Swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp;
 
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
